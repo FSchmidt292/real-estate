5f881c0b35577b1eeb660db664770cc8683ac8fd
Trey S <fjschmidt292@gmail.com>
2023-08-23 00:42:39 -0400

    beginning component mapping

---
+++ src/components/About/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function About() {
+    return (
+        <section>
+            <h1 id="about">This is the component for the about section to be injected at any place in the application!
+
+            </h1>
+        </section>
+    );
+}
+
+export default About;
